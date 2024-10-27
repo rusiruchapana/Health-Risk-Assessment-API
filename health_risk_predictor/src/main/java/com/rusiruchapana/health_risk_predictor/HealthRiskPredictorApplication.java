79b0120028cfe9a54ed78e7a74fb127d852429d3
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-28 01:35:17 +0530

    start the ML project.

---
+++ health_risk_predictor/src/main/java/com/rusiruchapana/health_risk_predictor/HealthRiskPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.rusiruchapana.health_risk_predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthRiskPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthRiskPredictorApplication.class, args);
+	}
+
+}
---
+++ health_risk_predictor/src/test/java/com/rusiruchapana/health_risk_predictor/HealthRiskPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusiruchapana.health_risk_predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthRiskPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
